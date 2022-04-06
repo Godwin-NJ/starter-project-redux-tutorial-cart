274c1b587be93a0fd16c7d814fa9c66ce8daeeb8
Godwin-NJ <amadigodwin7@gmail.com>
2022-04-06 17:44:54 +0100

    review react- redux

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React from "react";
 // components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { Provider } from "react-redux";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import { createStore } from "redux";
+import { cartReducer } from "./Reducer";
 
+const initialStore = {
+  cart: cartItems,
+  amount: 7,
+  total: 105,
+};
+
+let store = createStore(cartReducer, initialStore);
+console.log(store);
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+      <CartContainer />
+      {/* <CartContainer cart={cartItems} /> */}
+    </Provider>
   );
 }
 
